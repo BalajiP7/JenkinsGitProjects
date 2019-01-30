7fdb61948160873bcb8cc67b25089e4918afccfa
Probe7 <Probe7@PS10>
2019-01-30 10:44:50 +0530

    Test Jenkins Commit

---
+++ DemoJenkins/mytest.bat
@@ -0,0 +1,3 @@
+cd E:\Automation\New_Workspace\DemoJenkins
+
+java -cp E:\Automation\New_Workspace\DemoJenkins\lib*;E:\Automation\New_Workspace\DemoJenkins\bin org.testng.TestNG testng.xml
---
+++ DemoJenkins/src/DemoJenkinsPackage/FirstPgm.java
@@ -0,0 +1,11 @@
+package DemoJenkinsPackage;
+
+import org.testng.annotations.Test;
+
+public class FirstPgm {
+  @Test
+  public void f() {
+	  
+	  System.out.println("Welcome to Jenkins");
+  }
+}
